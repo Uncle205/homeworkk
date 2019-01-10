ed3f5d75f955a9d8dffca0224052aff745933298
амир абдрахманов <Uncle205@mail.ru>
2019-01-10 20:53:13 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+     
+            DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(ConfigurationManager.ConnectionStrings["AlphaConnectionStrings"].ProviderName);
+       
+
+        }
+    }
+}
